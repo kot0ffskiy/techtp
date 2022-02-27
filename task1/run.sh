8d883a36f0cefade8027ed0a3455560d8f364e13
Gurdan <subolugansk@gmail.com>
2022-02-27 17:13:27 +0300

    new backup-script run.sh

---
+++ task1/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while [ -n "$1" ]
+do
+case "$1" in
+--input_folder) inpf=$2 ;;          #абсолютный путь к директории 
+--extension) ext=$2 ;;              #расширение
+--backup_folder) bckf=$2 ;;         #название папки для бэкапа
+--backup_archive_name) bckar=$2 ;;  #имя архива с бэкапом
+esac
+shift 2
+done
+
+mkdir $bckf
+find $inpf -name "*.$ext" -exec cp '{}' ./$bckf \;
+tar -zcf $bckar $bckf
+
+echo done
