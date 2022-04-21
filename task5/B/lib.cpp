dbe16e8650c6986d993a9e2570556bb075b9f04a
gurdan <sushkov.br@phystech.edu>
2022-04-22 00:42:46 +0300

    Revert "first docker"
    This reverts commit c04a0a445e2412bac84367a4c8c77353c565ea9c.

--- task5/B/lib.cpp
+++
@@ -1,9 +0,0 @@
-#include "lib.h"
-
-void ClassLib::Set(int value) {
-    x_ = value;    
-}
-
-int ClassLib::Get() const {
-    return x_;
-}
--- task5/B/lib.h
+++
@@ -1,7 +0,0 @@
-class ClassLib {
- public:
-    void Set(int value);
-    int Get() const;
- private:
-    int x_;
-};
